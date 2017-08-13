5d1076b0f9f4ce0defcc1ede4650d1a91efe3438
Myca <joao.montez@gmail.com>
2017-08-13 16:26:34 +0200

    a

--- sketch_dec22a/sketch_dec22a.ino
+++ sketch_dec22a/sketch_dec22a.ino
@@ -4,139 +4,72 @@ byte IR_EN = 2;
 byte PR_IN = A1;
 byte PR_EN = 9;
 byte BP_OUT= 10;
-byte State = 255;
+
+byte BTN_IN = A4;
+byte PWR_IN = A5;
 
 short IR_AVG = 0;
 short PR_AVG = 0;
 
+short C_BT_SETUP_CONFIRM = 0x1FF1;
+short C_BT_SETUP_BLUETOOTH = 0x1FF2;
+
+int messageQueueLength=128;
+short messageQueue[128];//Arduino array lenghts must be compile-time constants
+byte messageQueueIndex=0;
+
+boolean debug=true;
+
+String UUID="";
 
 void setup() {
   Serial.begin(9600);
-  _pinConfig();
-  Calibrate_Sensor(IR_IN);
-  Calibrate_Sensor(PR_IN);
+  initMessageQueue();
+  initSetup();
+  initBluetooth();
+  waitForStart();
 }
 
-int Calibrate_Sensor(byte id){
-  short avg =0;
-  short _i=0;
-  byte target=_mapSensorToVar(id);  
-  while (_i < 512){
-    
-    _i = analogRead(target);    
-    avg = avg + _i;
-    if(_i > 0){
-      avg = avg / 2;
-    }
-    _i = _i +1;
-  }
-  
-  return avg;     
-}
+
 
 void loop() {
-	byte _newState = _pollChanges();
-	if (_newState != State){
-		State=_newState;
-		_pinConfig();
-	}
-	_stateBasedAction();
-
+  messageQueuePoll();
 }
 
-byte _pollChanges(){
-	int _in = -1;
-	if(Serial.available()){
-		
-	}
-	
-	return _in;
-}
 
-byte _mapSensorToVar(byte id){
-  byte t;
-  if(id == PR_IN){
-	digitalWrite(PR_EN, HIGH);
-    t=PR_IN;  
-  }
-  else if (id == IR_IN){
-    t=IR_IN;
-	digitalWrite(IR_EN, HIGH);
-  }
-  return t;
-}
-
-void _pinConfig(){
-		if(State==255 || State == 0){
-			pinMode(IR_IN, INPUT);
-			pinMode(PR_IN, INPUT);
-			pinMode(IR_EN, OUTPUT);
-			digitalWrite(IR_EN, LOW);	
-			digitalWrite(PR_EN, LOW);
-			State=0;
-		}
-		if(State==1){
-			digitalWrite(PR_EN, HIGH);
-		}
-		if(State==2){
-			digitalWrite(IR_EN, HIGH);
-			digitalWrite(PR_EN, LOW);
-		}
-		
-}
-
-byte BTRead(){
+short BTRead(){
 	return Serial.read();
 }
 
-void BTSend(byte data){
+void BTSend(short data){
 	if (Serial.availableForWrite()>0){
 		Serial.write(data);
 	}
+  else{
+    addToMessageQueue(data);
+  }
 }
 
 boolean _isBTAvailable(){
 	return Serial.available()>0;
 }
 
-
-void _stateBasedAction(){
-	if(State==0){
-		if (_isBTAvailable()){
-			byte _in = BTRead();
-			if(_in>0){
-				State=1;
-				_pinConfig();
-				BTSend(State);
-				return;
-			}
-		}
-	}
-	if(State==1){
-		_startCountDown();
-		
-	}
-	if(State==2){
-		_startAndCalcIR(millis());
-		State=0;
-	}
-	
-}
-
-void _beep(){
+void beep(int delayMillis){
 	BP_OUT=1;
-	delay(1);
+	if(delayMillis!=-1){
+	  delay(delayMillis);
+    BP_OUT=0;
+	}
 }
 
 void _startCountDown(){
-	_beep();
-	_beep();
-	_beep();	
+	beep(1000);
+	beep(1000);
+	beep(-1);	
 	BTSend(millis());
 	int _t = calcReactionTime(millis());
 	BTSend(_t);	
-	State=2;
-	_pinConfig();
+  BP_OUT=0;
 }
 
 int calcReactionTime(unsigned long startTime){
@@ -145,8 +78,8 @@ int calcReactionTime(unsigned long startTime){
 	do{
 		stop = checkForSensor(PR_IN, 0);
 		_interval = millis() - startTime;
-  } while(_interval < 5000 && !stop);
-   
+   debugLog("");
+  } while(_interval < 5000 || !stop);   
 	return _interval;	
 }
 
@@ -154,7 +87,7 @@ bool checkForSensor(int pin, int value){
 	return(analogReadToDigital(pin)==value);
 }
 
-bool analogReadToDigital(byte pin){
+int analogReadToDigital(byte pin){
 	short _in = analogRead(pin);
 	if(_in>2.5){
 		return 1;
@@ -162,14 +95,88 @@ bool analogReadToDigital(byte pin){
 	return 0;
 }
 
-void _startAndCalcIR(unsigned long startTime){
+void startAndCalcPR(unsigned long startTime){
 	int _interval=0;
   bool stop;
-	do{
-		stop = checkForSensor(IR_IN, 0);
+  while(_interval < 5000 && !stop){
+		stop = checkForSensor(PR_IN, 0);
 		_interval = millis() - startTime;
-	} while(_interval < 5000 && !stop);
+	} 
 	BTSend(_interval)	;
 }
 
+
+
+//messageQueueImpl
+
+void messageQueuePoll(){
+  if(messageQueue[messageQueueIndex]!=-1){
+    BTSend(messageQueue[messageQueueIndex]);
+    messageQueueIndex++;
+  }
+}
+
+void addToMessageQueue(short value){
+  messageQueue[messageQueueIndex]=value;
+  messageQueueIndex++;
+  if(messageQueueIndex>255){
+    messageQueueIndex=0;
+  }
+}
+
+//initial setup
+void waitForStart(){
+  debugLog("Waiting for pressure");
+  int _i = 0;
+  while(_i-=1){
+    boolean pressureValue = checkForSensor(PWR_IN, 1);
+    if(pressureValue){
+      _i=1;
+    }
+  }
+  beep(300);
+  beep(300);
+  debugLog("Pressure found");
+}
+
+void initSetup(){
+  debugLog("Setting up start");
+  int _i = 0;
+  while(_i-=1){
+    boolean isPowerButton = checkForSensor(PWR_IN, 1);
+    short bluetoothMessage = BTRead();
+    if(isPowerButton || bluetoothMessage==C_BT_SETUP_CONFIRM){
+      _i=1;
+    }
+  }
+  debugLog("Finished setting up");
+}
+
+void initBluetooth(){
+  debugLog("Setting up bluetooth");
+  int _i = 0;
+  while(_i-=1){
+    boolean isPowerButton = checkForSensor(PWR_IN, 1);
+    short bluetoothMessage = BTRead();
+    if(isPowerButton || bluetoothMessage==C_BT_SETUP_BLUETOOTH){
+      _i=1;
+    }
+  }
+  debugLog("Finished setting up bluetooth");
+  beep(1000);
+}
+
+void initMessageQueue(){
+  for(int i=0;i!=messageQueueLength;i++){
+    messageQueue[i]=-1;
+  }
+}
+
+//Debug or Temp
+
+void debugLog(String message){
+  if(debug){
+    Serial.print(message);
+  }
+}
 
